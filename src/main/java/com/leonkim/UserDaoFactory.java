089cd7b08615fa145673828f57256b3862b39894
pilhwanKim <pilhwankim03@gmail.com>
2017-03-10 18:31:48 +0900

    apply spring context

--- src/main/java/com/leonkim/UserDaoFactory.java
+++ src/main/java/com/leonkim/UserDaoFactory.java
@@ -1,16 +1,23 @@
 package com.leonkim;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by pilhwankim on 2017. 3. 10..
  */
+
+@Configuration
 public class UserDaoFactory {
-    public UserDao getLocalUserDao() {
+    @Bean
+    public UserDao localUserDao() {
         ConnectionMaker connectionMaker = new LocalConnectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
     }
 
-    public UserDao getPrdUserDao() {
+    @Bean
+    public UserDao prdUserDao() {
         ConnectionMaker connectionMaker = new PrdConnectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,4 +1,6 @@
 import com.leonkim.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -7,23 +9,28 @@ import java.sql.SQLException;
  */
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException {
-        String env = "local";
 
-        UserDaoFactory userDaoFactory = new UserDaoFactory();
+        // ApplicationContext 선언
+        ApplicationContext context;
+
+        // AnnotationConfigApplicationContext 생성
+        context = new AnnotationConfigApplicationContext(UserDaoFactory.class);
+
+        String env = "local";
         UserDao dao;
         if(env.equals("prd")) {
-            dao = userDaoFactory.getPrdUserDao();
+            dao = context.getBean("prdUserDao", UserDao.class);
         } else {
-            dao = userDaoFactory.getLocalUserDao();
+            dao = context.getBean("localUserDao", UserDao.class);
         }
 
         User user = new User();
-        user.setId("leon0517-1");
+        user.setId("leon0517-6");
         user.setName("pilhwan kim");
         user.setPassword("12345");
         dao.add(user);
 
-        User user2 = dao.get("leon0517-1");
+        User user2 = dao.get("leon0517-6");
         System.out.println("id:" + user2.getId());
         System.out.println("password:" + user2.getPassword());
         System.out.println("name:" + user2.getName());

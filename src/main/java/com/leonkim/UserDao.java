308e3147c81ccab14a34ea4e21b804cabe194f2a
pilhwanKim <pilhwankim03@gmail.com>
2017-03-10 16:44:05 +0900

    Apply extract method & template method pattern to userDao.

--- src/main/java/com/leonkim/UserDao.java
+++ src/main/java/com/leonkim/UserDao.java
@@ -5,10 +5,18 @@ import java.sql.*;
 /**
  * Created by pilhwankim on 2017. 3. 10..
  */
-public class UserDao {
+public abstract class UserDao {
+//
+//    private Connection getConnection() throws SQLException {
+//        Connection con = null;
+//        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_practice_db", "root", "");
+//        return con;
+//    }
+
+    abstract Connection getConnection() throws SQLException;
+
     public void add(User user) throws SQLException {
-        Connection con = null;
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_practice_db", "root", "");
+        Connection con = getConnection();
 
         PreparedStatement ps = con.prepareStatement("insert into users(id, password, name) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -22,8 +30,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        Connection con = null;
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_practice_db", "root", "");
+        Connection con = getConnection();
 
         PreparedStatement ps = con.prepareStatement("select * from users where id = ?");
         ps.setString(1,  id);
@@ -43,19 +50,19 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException {
-        UserDao dao = new UserDao();
+        UserDao dao = new PrdUserDao();
 
-//        User user = new User();
-//        user.setId("leon0517-3");
-//        user.setName("pilhwan kim");
-//        user.setPassword("12345");
-//        dao.add(user);
+        User user = new User();
+        user.setId("leon0517");
+        user.setName("pilhwan kim");
+        user.setPassword("12345");
+        dao.add(user);
 
-        User user = dao.get("leon0517-3");
+        User user2 = dao.get("leon0517");
 //
-        System.out.println("id:" + user.getId());
-        System.out.println("password:" + user.getPassword());
-        System.out.println("name:" + user.getName());
+        System.out.println("id:" + user2.getId());
+        System.out.println("password:" + user2.getPassword());
+        System.out.println("name:" + user2.getName());
     }
 
 }
